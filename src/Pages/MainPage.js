a4dc7b8e14606056d34e4bb50c66c1f7affc227c
paulsoh <yongdoree@gmail.com>
2017-03-30 07:13:48 +0900

    Test basic firebase functionality

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import firebase from '../firebase';
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      snapshot: {},
+    }
+
+    this.firebase = firebase.database().ref();
+
+    this.firebase.on('value', snapshot => {
+      console.log('value changed');
+      console.log(snapshot.val());
+      const store = snapshot.val();
+
+      this.setState({
+        snapshot: store,
+      })
+    });
+  }
+
   render() {
+    console.log(this.state.snapshot);
     return (
       <div>
         Main Page
