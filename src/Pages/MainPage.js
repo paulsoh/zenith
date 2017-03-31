94c76b29748ed95cb0896a339e709bf4e840bd74
Paul Yongseok Soh <yongdoree@gmail.com>
2017-03-31 17:49:53 +0900

    Add style

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -24,8 +24,30 @@ class MainPage extends React.Component {
   render() {
     console.log(this.state.snapshot);
     return (
-      <div>
-        Main Page
+      <div
+        className="gray-bg"
+      >
+        <div className="row wrapper border-bottom white-bg page-heading">
+          <div className="col-lg-10">
+            <h2>
+              Vocabulary List
+            </h2>
+            <ol className="breadcrumb">
+              <li>GRE</li>
+            </ol>
+          </div>
+        </div>
+        <div className="wrapper wrapper-content animated fadeInRight">
+          <div className="row m-b-lg m-t-lg">
+            <div className="col-md-6">
+              GRE Full Vocabulary
+            </div>
+            <div className="col-md-3">
+            </div>
+            <div className="col-md-3">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './Style/build/bootstrap.css';
+import './Style/build/animate.css';
+import './Style/build/style.css';
 import './index.css';
 import './firebase';
 
